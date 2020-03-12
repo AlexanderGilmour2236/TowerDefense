2b4248b9cf5d23431840385f120b5206e7ea6481
SerebriakovAlex <serebriakovalex2236@gmail.com>
2020-03-12 11:18:10 +0200

    Add towers prefabs

--- Assets/Scripts/Towers/Views/TowerView.cs
+++ Assets/Scripts/Towers/Views/TowerView.cs
@@ -4,10 +4,26 @@ namespace Towers.Views
 {
     public class TowerView : MonoBehaviour
     {
+        private Transform _target;
         public TowerData TowerData { get; private set; }
         public void SetData(TowerData towerData)
         {
             TowerData = towerData;
+        }
+
+        public void SetTarget(Transform target)
+        {
+            
+        }
+
+        private void Update()
+        {
+            if (_target == null)
+            {
+                transform.LookAt(Vector3.zero);
+                return;
+            }
+            transform.LookAt(_target);
         }
     }
 }
